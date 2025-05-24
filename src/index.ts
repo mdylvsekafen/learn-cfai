7f9984721778f7d9f5d9a28b2a96fc65465167cb
mdylvsekafen <lvsekafen@outlook.com>
2025-05-24 10:19:33 +0000

    remove upload code

--- src/index.ts
+++ src/index.ts
@@ -64,111 +64,14 @@ export default {
 
       const response = await env.AI.run(model, inputs);
 
+      // 直接返回原始图片
+      return new Response(response, {
+        headers: {
+          ...corsHeaders,
+          'content-type': 'image/png;base64',
+        },
+      });
 
-      if (model === '@cf/black-forest-labs/flux-1-schnell') {
-        // flux-1-schnell模型返回的是base64编码，其他模型返回的都是二进制的png文件
-        // 如果模型是 flux-1-schnell，处理 base64 图片数据
-        if (response.image) {
-          const base64Image = response.image;
-          const blob = await base64ToBlob(base64Image);
-
-          if (upload) {
-            const uploadResponse = await uploadToImageHost(blob);
-            if (uploadResponse?.src) {
-              // 返回图床的图片地址
-              return new Response(JSON.stringify({
-                imageUrl: `https://pic.foxhank.top${uploadResponse.src}`
-              }), {
-                status: 200,
-                headers: {
-                  ...corsHeaders,
-                  'content-type': 'application/json'
-                }
-              });
-            } else {
-              return new Response(JSON.stringify({
-                errors: ['Image upload failed'],
-                messages: []
-              }), {
-                status: 500,
-                headers: {
-                  ...corsHeaders,
-                  'content-type': 'application/json'
-                }
-              });
-            }
-          } else {
-            // 不上传，直接返回原始图片
-            return new Response(blob, {
-              headers: {
-                ...corsHeaders,
-                'content-type': 'image/png'
-              }
-            });
-          }
-        } else {
-          return new Response(JSON.stringify({
-            errors: ['No image found in the response'],
-            messages: []
-          }), {
-            status: 500,
-            headers: {
-              ...corsHeaders,
-              'content-type': 'application/json'
-            }
-          });
-        }
-      } else {
-        //处理其他模型
-        if (upload) {
-          //把返回的stream转化为blob
-          async function convertReadableStreamToBlob(readableStream: BodyInit | null | undefined) {
-            try {
-              // 将 ReadableStream 转换为 Blob
-              const response = new Response(readableStream);
-              const blob = await response.blob();
-              return blob;
-            } catch (error) {
-              console.error('转换 ReadableStream 出错:', error);
-              return null;
-            }
-          }
-          // 转换 ReadableStream 为 Blob
-          const blob = await convertReadableStreamToBlob(response);
-
-          const uploadResponse = await uploadToImageHost(blob);
-          if (uploadResponse?.src) {
-            return new Response(JSON.stringify({
-              imageUrl: `https://pic.foxhank.top${uploadResponse.src}`
-            }), {
-              status: 200,
-              headers: {
-                ...corsHeaders,
-                'content-type': 'application/json'
-              }
-            });
-          } else {
-            return new Response(JSON.stringify({
-              errors: ['Image upload failed'],
-              messages: [uploadResponse.text]
-            }), {
-              status: 500,
-              headers: {
-                ...corsHeaders,
-                'content-type': 'application/json'
-              }
-            });
-          }
-        } else {
-          // 如果不上传，直接返回原始图片
-          return new Response(response, {
-            headers: {
-              ...corsHeaders,
-              'content-type': 'image/png;base64',
-            },
-          });
-        }
-      }
     } else {
       // 处理 GET 请求，返回 index.html
       return new Response(HTML.replace(/{{host}}/g, originalHost), {
@@ -194,26 +97,3 @@ async function base64ToBlob(base64: string) {
   return new Blob([uint8Array], { type: 'image/png' });
 }
 
-// 上传图片到图床
-async function uploadToImageHost(blob: Blob) {
-  const formData = new FormData();
-  formData.append('file', blob, 'image.png');
-
-  try {
-    const response = await fetch('https://pic.foxhank.top/upload', {
-      method: 'POST',
-      body: formData,
-    });
-
-    if (!response.ok) {
-      const errorMessage = await response.text(); // 等待并获取错误信息
-      throw new Error(`HTTP error! status: ${response.status}, message: ${errorMessage}`);
-    }
-
-    const jsonResponse = await response.json();
-    return jsonResponse[0]; // 图床返回地址里面是 src 字段
-  } catch (error) {
-    console.error(error); // 记录错误信息
-    return null;
-  }
-}
